47d1c9f5ac655379a4dcefa91be8e7aa961ae36e
OUEDRAOGO Souleymane <soulouedraogo0844@gmail.com>
2026-05-05 02:13:23 +0000

    Ajout de la fonction generer_facture

--- facturation.py
+++ facturation.py
@@ -4,3 +4,7 @@ def calculer_total(prix, quantite):
 def appliquer_remise(prix, remise):
     total = calculer_total(prix, 1)
     return total - remise
+
+def generer_facture(client, prix, quantite):
+    total = calculer_total(prix, quantite)
+    return {"client": client, "total": total}
