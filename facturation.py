701dcb8e663d121d1d8e816feca38c9bbe62316f
OUEDRAOGO Souleymane <soulouedraogo0844@gmail.com>
2026-07-14 09:35:59 +0000

    nettoyage...

--- facturation.py
+++
@@ -1,6 +0,0 @@
-def calculer_total_TTC(prix, quantite, taux_tva=0.2):
-    return prix * quantite * (1 + taux_tva)
-
-def appliquer_remise(prix, remise):
-    total = calculer_total_TTC(prix, 1)
-    return total - remise
